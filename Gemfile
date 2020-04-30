cb82537dc4d48b98cb3f6ef5537fa0ba1b280950
Kyle Tan <kylebrandontan@gmail.com>
2020-04-30 21:53:04 +0800

    Add models and fast json api

--- Gemfile
+++ Gemfile
@@ -4,7 +4,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby '2.6.3'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '~> 5.2.4', '>= 5.2.4.1'
+gem 'rails'
 # Use postgresql as the database for Active Record
 gem 'pg', '>= 0.18', '< 2.0'
 # Use Puma as the app server
@@ -28,7 +28,7 @@ gem 'jbuilder', '~> 2.5'
 # gem 'redis', '~> 4.0'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
-
+gem 'fast_jsonapi'
 # Use ActiveStorage variant
 # gem 'mini_magick', '~> 4.8'
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,36 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+airlines = Airline.create([
+  {
+    name: "United Airlines",
+    image_url: "https://open-flights.s3.amazonaws.com/United-Airlines.png"
+  },
+  {
+    name: "Philippine Airlines",
+    image_url: "#"
+  },
+  {
+    name: "Canada Airlines",
+    image_url: "#"
+  },
+  {
+    name: "Delta Airlines",
+    image_url: "#"
+  }
+])
+
+reviews = Review.create([
+  {
+    title: 'great airline',
+    description: 'good',
+    score: 5,
+    airline: airlines.first
+  },
+  {
+    title: 'bad airline',
+    description: 'horrible',
+    score: 1,
+    airline: airlines.first
+  }
+])
